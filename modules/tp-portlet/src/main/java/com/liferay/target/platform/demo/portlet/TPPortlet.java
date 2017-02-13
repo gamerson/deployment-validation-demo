dd2c6673efdf9ed522863f6e72660e8d28bb4f36
Gregory Amerson <gregory.amerson@liferay.com>
2017-02-13 11:19:24 -0600

    fix gitignores

---
+++ modules/tp-portlet/src/main/java/com/liferay/target/platform/demo/portlet/TPPortlet.java
@@ -0,0 +1,26 @@
+package com.liferay.target.platform.demo.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Greg
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=tp-portlet Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class TPPortlet extends MVCPortlet {
+}
